e7670b9a2e9b02b1b199508b308908667ce0292a
Richard Zhan <rzhan@andrew.cmu.edu>
2023-03-21 23:15:26 -0400

    added explore

--- nbs/log_parser.py
+++ nbs/log_parser.py
@@ -2,33 +2,33 @@ import json
 import sys
 from pathlib import Path
 
-# def process_file(fname, start_sep, end_sep):
-
-#     input_file = Path(fname)
-
-#     print("Reading from input file", input_file)
-#     with open(input_file, "r") as f:
-#         text = f.read()
-
-#     # split on start_sep
-#     splits = text.split(start_sep)
-
-#     # skip first entry (before __mark)
-#     splits = splits[1:]
-
-#     # split on end_sep
-#     splits = [s.split(end_sep) for s in splits]
-
-#     # # only keep before end_sep
-#     splits = [s[0] for s in splits]
-#     splits = [json.loads(s) for s in splits]
-
-
-
-#     return splits
-
-
 def process_file(fname, start_sep, end_sep):
+
+    input_file = Path(fname)
+
+    print("Reading from input file", input_file)
+    with open(input_file, "r") as f:
+        text = f.read()
+
+    # split on start_sep
+    splits = text.split(start_sep)
+
+    # skip first entry (before __mark)
+    splits = splits[1:]
+
+    # split on end_sep
+    splits = [s.split(end_sep) for s in splits]
+
+    # # only keep before end_sep
+    splits = [s[0] for s in splits]
+    splits = [json.loads(s) for s in splits]
+
+
+
+    return splits
+
+
+def process_file_jasper(fname, start_sep, end_sep):
 
     input_file = Path(fname)
 
